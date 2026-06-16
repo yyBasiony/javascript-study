eeb307e673bfaf696fdef25743675015861d8dfb
yasmine ibrahim <iayasmine34@gmail.com>
2026-06-16 14:51:43 +0300

    start learning java script

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("hello"); //hello
+var x = 10;
+console.log(x); //10
+console.log("x"); //x
+x = 20;
+console.log(x); //20
+var X = "yasmine";
+console.log(X);
+var firstNum = 10;
+var secondNum = 4;
+var sum = firstNum + secondNum;
+var sub = firstNum + secondNum;
+var mult = firstNum * secondNum;
+var div = firstNum / secondNum;
+var mod = firstNum % secondNum;
+console.log("First number is" + firstNum);
+console.log("Second number is" + secondNum);
+console.log("Sum is" + sum);
+console.log("Subtraction is" + sub);
+console.log("Multiplication is" + mult);
+console.log("Division is" + div);
+console.log("Modulus is" + mod);
